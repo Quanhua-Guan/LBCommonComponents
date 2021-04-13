2a5c5b354845fb6228232ca349c3e8a2195dc0b9
刘彬 <1129434577@qq.com>
2021-04-13 15:27:59 +0800

    修改self.view.backgroundColor颜色

--- LBCommonComponents/Base/ViewControllers/UIViewController+LBNavigationBarAppearance.m
+++ LBCommonComponents/Base/ViewControllers/UIViewController+LBNavigationBarAppearance.m
@@ -58,20 +58,27 @@ static NSString *LBNavigationBarTintColorKey = @"LBNavigationBarTintColorKey";
 
 -(void)lb_navigationBarAppearance_viewDidLoad{
     if ([NSStringFromClass(self.class) containsString:@"UI"] == NO) {
-        if (@available(iOS 13.0, *)) {
-            if ([UITraitCollection currentTraitCollection].userInterfaceStyle == UIUserInterfaceStyleLight) {
-                if (self.view.backgroundColor == nil) {
-                    self.view.backgroundColor = [UIColor whiteColor];
-                }
-            }else{
-                if (self.view.backgroundColor == nil) {
-                    self.view.backgroundColor = [UIColor blackColor];
-                }
-            }
-        }else{
-            if (self.view.backgroundColor == nil) {
-                self.view.backgroundColor = [UIColor whiteColor];
-            }
+//        if (@available(iOS 13.0, *)) {
+//            if ([UITraitCollection currentTraitCollection].userInterfaceStyle == UIUserInterfaceStyleLight) {
+//                if (self.view.backgroundColor == nil) {
+//                    self.view.backgroundColor = [UIColor whiteColor];
+//                }
+//            }else{
+//                if (self.view.backgroundColor == nil) {
+//                    self.view.backgroundColor = [UIColor blackColor];
+//                }
+//            }
+//        }else{
+//            if (self.view.backgroundColor == nil) {
+//                self.view.backgroundColor = [UIColor whiteColor];
+//            }
+//        }
+        if (self.view.backgroundColor == nil) {
+#ifdef __IPHONE_13_0
+            self.view.backgroundColor = [UIColor systemGroupedBackgroundColor];
+#else
+            self.view.backgroundColor = [UIColor groupTableViewBackgroundColor];
+#endif
         }
     }
     [self lb_navigationBarAppearance_viewDidLoad];
